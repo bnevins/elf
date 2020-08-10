d4e8202e52b2212675fe9a02f1f1f14efaaa0850
Byron Nevins <bnevins@Byrons-MacBook.hsd1.ca.comcast.net>
2020-08-09 17:30:06 -0700

    return -1 if not found

--- elf-commons/src/main/java/com/elf/util/sort/BinarySearch.java
+++ elf-commons/src/main/java/com/elf/util/sort/BinarySearch.java
@@ -6,73 +6,81 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package com.elf.util.sort;
 
 /**
  *
  * @author bnevins
  */
-public class BinarySearch
-{
+public class BinarySearch {
 
-    public BinarySearch(int[] a, int elem)
-    {
+    public BinarySearch(int[] a, int elem) {
         this.a = a;
         this.elem = elem;
     }
 
-    public int search()
-    {
-        search(0, a.length - 1, elem);
-        return numSearches;
+    public boolean search() {
+        return search(0, a.length - 1, elem);
     }
-    
 
-    private static int[] getSortedArray(int num)
-    {
+    private static int[] getSortedArray(int num) {
         int[] a = new int[num];
-        for(int i = 0; i < num; i++)
-        {
+        for (int i = 0; i < num; i++) {
             a[i] = i;
         }
-        
+
         return a;
     }
 
-    private boolean search(int lo, int hi, int elem)
-    {
+    private boolean search(int lo, int hi, int elem) {
         ++numSearches;
         System.out.println("Lo:Hi " + lo + " : " + hi);
-        if(lo > hi)
-        {
+        if (lo > hi) {
             System.out.println("ERROR");
         }
-        
-        if(lo == hi)
-        {
+
+        if (lo == hi) {
+            debug("Search found (lo == high) for " + elem + " at element #" + lo);
             return a[lo] == elem;
         }
-        
+
         int mid = (lo + hi) >>> 1;
-        
-        if(elem > a[mid])
+
+        if (elem > a[mid]) {
             return search(mid + 1, hi, elem);
-        if(elem < a[mid])
+        }
+        if (elem < a[mid]) {
             return search(lo, mid - 1, elem);
-        else
+        } else {
+            debug("Match found (mid) for " + elem + " at element #" + mid);
             return true;
+        }
     }
-    public static void main(String[] args)
-    {
-        int num = 200000000;
+
+    public static void main(String[] args) {
+        final int num = 200 * 1000 * 1000;
         System.out.println("NUM " + num);
-        int elem = 73;
-        
+        int elem = 200 * 1000 * 1000 + 1;
+
         BinarySearch search = new BinarySearch(getSortedArray(num), elem);
-        System.out.println("search returned: " + search.search());
+        boolean found = search.search();
+        if (!found) {
+            System.out.println("Can't find item!");
+        } else {
+            System.out.println("Found item!");
+        }
+    }
+
+    private void debug(String string) {
+        if (debug) {
+            System.out.println(string);
+        }
     }
     private int[] a;
     private int elem;
     private int numSearches = 0;
+
+    // set this to true to debug...
+    private boolean debug = true;
+
 }
