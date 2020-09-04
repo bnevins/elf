968757312ff686df1cdc6713917d75c8802c0400
Byron Nevins <biz@bnevins.com>
2020-09-04 12:22:43 -0700

    Playing with searching and sorting...

--- elf-commons/src/main/java/com/elf/util/sort/BinaryTree.java
+++ elf-commons/src/main/java/com/elf/util/sort/BinaryTree.java
@@ -1,83 +1,94 @@
-/*
- * BinaryTree.java
- *
- * Created on November 9, 2007, 10:56 PM
- *
- */
-
 package com.elf.util.sort;
 
-import java.io.*;
-import java.util.*;
+import java.util.Map;
 
 /**
  *
- * @author bnevins
+ * @author bnevns
+ * @param <Key>
+ * @param <Value>
  */
-public class BinaryTree
-{
-    public BinaryTree()
-    {
+public class BinaryTree<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        Node left;
+        Node right;
+        Key key;
+        Value value;
+        int numNodes;
+
+        Node(Key aKey, Value aValue, int n) {
+            key = aKey;
+            value = aValue;
+            numNodes = n;
+        }
     }
 
-    public boolean add(int value)
-    {
-        BinaryTreeItem node = root;
-        BinaryTreeItem item = new BinaryTreeItem(value);
+    public int size() {
+        return size(root);
+    }
 
-        if(node == null)
-        {
-            root = item;
-            return true;
+    public int size(Node node) {
+        if (node == null) {
+            return 0;
         }
-        
-        while(true)
-        {
-            int cmp = value - node.value;
-            
-            if(cmp == 0)
-                return false;
-            
-            else if(cmp < 0)
-            {
-                if(node.left == null)
-                {
-                    node.left = item;
-                    return true;
-                }
-                else
-                    node = node.left;
-            }
-         
-            else if(cmp > 0)
-            {
-                if(node.right == null)
-                {
-                    node.right = item;
-                    return true;
-                }
-                node = node.right;
-            }
-        }            
+        return node.numNodes;
     }
-    
-    public static void main(String[] args)
-    {
-        /*
-        Console cons = System.console();
-        BinaryTree tree = new BinaryTree();
-        
-        while(true)
-        {
-            String s = cons.readLine("Enter a number to insert: ");
-            System.out.println("Entered: " + s);
-         
-            if(s.equals("-1"))
-                System.exit(0);
-            
-            tree.add(Integer.parseInt(s));
+
+    public void put(Key k, Value v) {
+        root = put(root, k, v);
+    }
+
+    public Value get(Key k) {
+        return get(root, k);
+    }
+
+    private Value get(Node node, Key k) {
+        if (node == null) {
+            return null;
         }
-         */
+        int cmp = k.compareTo(node.key);
+
+        if (cmp < 0) {
+            System.out.println(node.key + "  <0");
+            return get(node.left, k);
+        } else if (cmp > 0) {
+            System.out.println(node.key + "  >0");
+            return get(node.right, k);
+        } else {
+            System.out.println(node.key + "  ==");
+
+            return node.value;
+        }
     }
-    BinaryTreeItem root;
-}    
+
+    private Node put(Node node, Key k, Value v) {
+        if (node == null) {
+            return new Node(k, v, 1);
+        }
+
+        int cmp = k.compareTo(node.key);
+
+        if (cmp < 0) {
+            node.left = put(node.left, k, v);
+        } else if (cmp > 0) {
+            node.right = put(node.right, k, v);
+        } else {
+            node.value = v;
+        }
+        node.numNodes = size(node.left) + size(node.right) + 1;
+        return node;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree<String, Integer> tree = new BinaryTree<>();
+        tree.put("hello", 25);
+        tree.put("goodbye", 5);
+        tree.put("foo", 3);
+        tree.put("foo", 3);
+        tree.put("ccc", 1);
+        tree.put("aaa", 1);
+        tree.get("fooo");
+    }
+}
