17c5510a3ba213f5dfd49eebf2a78e86182be60f
Byron Nevins <biz@bnevins.com>
2020-10-08 18:17:08 -0700

    Whitespace only!!

--- elf-apps/src/main/java/com/elf/graphics/ArtInfo.java
+++ elf-apps/src/main/java/com/elf/graphics/ArtInfo.java
@@ -3,29 +3,26 @@ package com.elf.graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
-class ArtInfo
-{
-    public static void main(String[] args) 
-	{
-		for(int i = 0; i < args.length; i++)
-		{
-			dumpInfo(args[i]);
-		}
-		System.exit(0);
+class ArtInfo {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            dumpInfo(args[i]);
+        }
+        System.exit(0);
     }
 
     /**
      * Reads an image in a file and dumps info about it.
+     *
      * @param name The name of image file.
      */
-    public static void dumpInfo(String name)
-	{
-		// Get the image from a file.
+    public static void dumpInfo(String name) {
+        // Get the image from a file.
         Image inImage = new ImageIcon(name).getImage();
 
-		int h = inImage.getHeight(null);
-		int w = inImage.getWidth(null);
-		System.out.println(name + " -- width: " + w + ", Height: " + h);				
+        int h = inImage.getHeight(null);
+        int w = inImage.getWidth(null);
+        System.out.println(name + " -- width: " + w + ", Height: " + h);
     }
 }
-    
