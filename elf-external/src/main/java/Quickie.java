870ab3845aec55c7fea41959deb33bf85ee4d76a
Byron Nevins <biz@bnevins.com>
2020-11-04 08:36:16 -0800

    Throwaway code

--- elf-external/src/main/java/Quickie.java
+++ elf-external/src/main/java/Quickie.java
@@ -1,3 +1,9 @@
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,8 +19,19 @@ public class Quickie {
     /**
      * @param args the command line arguments
      */
+    public Quickie() {
+        try {
+            InputStream in = getClass().getClassLoader().getResourceAsStream("data/tinyCG.txt");
+            byte[] bytes = in.readAllBytes();
+            String s = new String(bytes);
+            System.out.println(s);
+        }
+        catch (IOException ex) {
+            Logger.getLogger(Quickie.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Quickie();
     }
     
 }
