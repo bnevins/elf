23f58fd82f0205151c9aace3c630fab68fa53010
bnevins <biz@bnevins.com>
2023-03-12 11:00:28 -0400

    First Checkin!

---
+++ JShowart/src/main/java/com/elf/jshowart/JShowart.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.elf.jshowart;
+
+/**
+ *
+ * @author bnevins
+ */
+public class JShowart {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
