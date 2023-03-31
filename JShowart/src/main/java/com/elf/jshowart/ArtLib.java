7ba2e1672f52fafca1786b7e34b8b1e85175fd2b
bnevins <biz@bnevins.com>
2023-03-31 09:46:31 -0400

    Initial

---
+++ JShowart/src/main/java/com/elf/jshowart/ArtLib.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.elf.jshowart;
+
+/**
+ *
+ * @author bnevins
+ */
+// SINGLETON class
+public class ArtLib {
+    
+}
