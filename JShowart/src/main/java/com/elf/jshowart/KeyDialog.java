a7493c8df6017e77fc6218c87679faa5275807f2
bnevins <biz@bnevins.com>
2023-04-21 13:34:24 -0400

    First part done! Next:  hook up data in and out to prefs

--- JShowart/src/main/java/com/elf/jshowart/KeyDialog.java
+++ JShowart/src/main/java/com/elf/jshowart/KeyDialog.java
@@ -14,12 +14,11 @@ import java.awt.event.*;
  * @author bnevins
  */
 public class KeyDialog extends JPanel {
-
-    private final boolean DEBUG = true;
+    private UserPreferences prefs = UserPreferences.get();
     private final int FILE_OPERATION_COLUMN = 0;
-    private final int KEY_COLUMN = 4;
-    private final String FILE_OPERATION_TYPES[] = { "Copy", "Move", "List", "Index", };
-    private final String[] COLUMN_NAMES = {"Type", "Ctrl", "Shift", "Alt", "Key", "RelativeTo", "Target", };
+    private final int RELATIVE_TO_COLUMN = 5;
+    private final String FILE_OPERATION_TYPES[] = {"Copy", "Move", "List", "Index",};
+    private final String RELATIVE_TO_ITEMS[] = {"Root", "Current File", "Absolute",};
 
     public KeyDialog() {
         super(new GridLayout(1, 0));
@@ -32,118 +31,74 @@ public class KeyDialog extends JPanel {
         JScrollPane scrollPane = new JScrollPane(table);
 
         //Set up column sizes.
-        //initColumnSizes(table);
+        initColumnSizes(table);
         //Fiddle with the Type column's cell editors/renderers.
         setUpTypeColumn(table, table.getColumnModel().getColumn(FILE_OPERATION_COLUMN));
+        setUpRelativeToColumn(table, table.getColumnModel().getColumn(RELATIVE_TO_COLUMN));
 
         add(scrollPane);
     }
 
- 
     public void setUpTypeColumn(JTable table, TableColumn typeColumn) {
-        //Set up the editor for the Type cells.
         JComboBox comboBox = new JComboBox(FILE_OPERATION_TYPES);
         typeColumn.setCellEditor(new DefaultCellEditor(comboBox));
+        typeColumn.setCellRenderer(getToolTipRenderer());
+    }
 
-        //Set up tool tips for the sport cells.
-        DefaultTableCellRenderer renderer
-                = new DefaultTableCellRenderer();
+    public void setUpRelativeToColumn(JTable table, TableColumn typeColumn) {
+        var comboBox = new JComboBox(RELATIVE_TO_ITEMS);
+        typeColumn.setCellEditor(new DefaultCellEditor(comboBox));
+        typeColumn.setCellRenderer(getToolTipRenderer());
+    }
+
+    private DefaultTableCellRenderer getToolTipRenderer() {
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
         renderer.setToolTipText("Click for combo box");
-        typeColumn.setCellRenderer(renderer);
+        return renderer;
     }
+private void initColumnSizes(JTable table) {
+        var model = (KeyCommandTableModel)table.getModel();
+        TableColumn column = null;
+        Component comp = null;
+        int headerWidth = 0;
+        int cellWidth = 0;
+        Object[] longValues = model.longValues;
+        TableCellRenderer headerRenderer =
+            table.getTableHeader().getDefaultRenderer();
 
-    class KeyCommandTableModel extends AbstractTableModel {
+        for (int i = 0; i < longValues.length; i++) {
+            column = table.getColumnModel().getColumn(i);
 
-        private Object[][] data = {
-            {"Copy", Boolean.FALSE,Boolean.TRUE,Boolean.FALSE, KeyEvent.VK_F, "Root", "_best"},
-        };
+            comp = headerRenderer.getTableCellRendererComponent(
+                                 null, column.getHeaderValue(),
+                                 false, false, 0, 0);
+            headerWidth = comp.getPreferredSize().width;
 
-        public final Object[] longValues = {"Jane", "Kathy",
-            "None of the above",
-            new Integer(20), Boolean.TRUE};
-
-        public int getColumnCount() {
-            return COLUMN_NAMES.length;
-        }
-
-        public int getRowCount() {
-            return data.length;
-        }
-
-        public String getColumnName(int col) {
-            return COLUMN_NAMES[col];
-        }
-
-        public Object getValueAt(int row, int col) {
-            if(col == KEY_COLUMN) {
-                // Virtual Keys are just a number.  Return a String...
-                Integer key = (Integer)data[row][col];
-                return KeyEvent.getKeyText(key);
+            Class<?> clazz = model.getColumnClass(i);
+            Object longestValue = longValues[i];
+            
+// KLUDGE - special handling for Virtual Key Code - which is the only Integer in the Table
+            // DOES NOT WORK!! if(clazz.equals(Integer.class)) {
+            if(i == KeyCommandTableModel.KEY_COLUMN) {
+                clazz = String.class;
+                longestValue = KeyEvent.getKeyText((Integer)longestValue);
             }
-            return data[row][col];
-        }
+            
+            comp = table.getDefaultRenderer(clazz).getTableCellRendererComponent(table, longestValue,false, false, 0, i);
+            cellWidth = comp.getPreferredSize().width;
 
-        /*
-         * JTable uses this method to determine the default renderer/
-         * editor for each cell.  If we didn't implement this method,
-         * then the boolean columns would contain text ("true"/"false"),
-         * rather than a check box.
-         */
-        public Class getColumnClass(int c) {
-            return getValueAt(0, c).getClass();
-        }
-
-
-        /*
-         * Don't need to implement this method unless your table's
-         * editable.
-         */
-        public boolean isCellEditable(int row, int col) {
-            //Note that the data/cell address is constant,
-            //no matter where the cell appears onscreen.
-//            if (col < 2) {
-//                return false;
-//            } else {
-                return true;
-//            }
-        }
-
-        /*
-         * Don't need to implement this method unless your table's
-         * data can change.
-         */
-        public void setValueAt(Object value, int row, int col) {
-            if (DEBUG) {
-                System.out.println("Setting value at " + row + "," + col
-                        + " to " + value
-                        + " (an instance of "
-                        + value.getClass() + ")");
+            if (prefs.isDebug()) {
+                System.out.println("Initializing width of column "
+                                   + i + ". "
+                                   + "headerWidth = " + headerWidth
+                                   + "; cellWidth = " + cellWidth);
             }
 
-            data[row][col] = value;
-            fireTableCellUpdated(row, col);
-
-            if (DEBUG) {
-                System.out.println("New value of data:");
-                printDebugData();
-            }
+            column.setPreferredWidth(Math.max(headerWidth, cellWidth));
         }
+}
 
-        private void printDebugData() {
-            int numRows = getRowCount();
-            int numCols = getColumnCount();
-
-            for (int i = 0; i < numRows; i++) {
-                System.out.print("    row " + i + ":");
-                for (int j = 0; j < numCols; j++) {
-                    System.out.print("  " + data[i][j]);
-                }
-                System.out.println();
-            }
-            System.out.println("--------------------------");
-        }
-    }
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
